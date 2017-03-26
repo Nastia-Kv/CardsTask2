068e1852ab6a6d197dc42c48a44ac297bb1f5973
Nastia-Kv <nastia.kvas.89@gmail.com>
2017-03-26 12:16:54 +0300

    first commit

---
+++ src/com/lohika/Rank.java
@@ -0,0 +1,14 @@
+package com.lohika;
+
+public class Rank {
+
+    private final String name;
+    private final String sign;
+
+    public Rank(String name, String sign) {
+        this.name = name;
+        this.sign = sign;
+    }
+
+
+}
